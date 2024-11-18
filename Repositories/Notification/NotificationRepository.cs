7289497f11b1059050e79c294b7c033e5c6e8d1d
Gabriel Herrera Waunous <pablogabrielherrera@gmail.com>
2024-11-18 20:25:00 -0300

    Add feature get notification id

--- Repositories/Notification/NotificationRepository.cs
+++ Repositories/Notification/NotificationRepository.cs
@@ -1,15 +1,28 @@
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
+using Dapper;
 
-public class NotificationRepository : INotificationRepository
+public class NotificationRepository(IDbConnection dbConnection) : INotificationRepository
 {
+    private readonly IDbConnection _dbConnection = dbConnection;
     public Task<IEnumerable<Notification>> GetNotificacionByUser(int userId)
     {
         throw new System.NotImplementedException();
     }
-    public Task<Notification> GetNotificacionById(int id)
+    public async Task<Notification> GetNotificacionById(int id)
     {
-        throw new System.NotImplementedException();
+        var query = @"
+            SELECT [id]
+                ,[user_id]
+                ,[message]
+                ,[read]
+            FROM [ProjectManagementDB].[dbo].[NOTIFICATIONS]
+            WHERE [id] = @id
+        ";
+
+        var notification  = await _dbConnection.QueryFirstOrDefaultAsync<Notification>(query, new { id = id });
+        return notification;
     }
     public Task<Notification> CreateNotification(Notification notification)
     {
