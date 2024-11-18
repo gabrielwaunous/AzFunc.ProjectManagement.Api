128a0a587862089fc93e474868a6604a22bf1d3b
Gabriel Herrera Waunous <pablogabrielherrera@gmail.com>
2024-11-18 19:43:50 -0300

    Add feature Notification (#25)

---
+++ Functions/NotificationFunction.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace PersonalProjects.Function
+{
+    public class NotificationFunction
+    {
+        private readonly ILogger<NotificationFunction> _logger;
+
+        public NotificationFunction(ILogger<NotificationFunction> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("NotificationFunction")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Repositories/Notification/NotificationRepository.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class NotificationRepository : INotificationRepository
+{
+    public Task<IEnumerable<Notification>> GetNotificacionByUser(int userId)
+    {
+        throw new System.NotImplementedException();
+    }
+    public Task<Notification> GetNotificacionById(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+    public Task<Notification> CreateNotification(Notification notification)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<Notification> UpdateNotification(Notification notification)
+    {
+        throw new System.NotImplementedException();
+    }
+    public Task<bool> DeleteNotification(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+}
